aba22f7ba06cf3ff7d21d289c47b790351dc8531
Валера <kolesnikovvalerij275@gmail.com>
2025-10-28 14:22:39 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1: демонстрация методов
+            Console.WriteLine("Сумма чисел: " + Sum(5, 3));
+
+            int number = 10;
+            CheckNumber(number);
+
+            // Задание 2: работа с классом Пользователь
+            User user1 = new User();
+            user1.Id = 1;
+            user1.Name = "Иван";
+            user1.Email = "ivan@mail.ru";
+            user1.Age = 25;
+
+            user1.PrintInfo();
+
+            User user2 = new User
+            {
+                Id = 2,
+                Name = "Мария",
+                Email = "maria@yandex.ru",
+                Age = 22
+            };
+
+            Console.WriteLine(user2.GetUserData());
+        }
+
+        // Public метод с возвращаемым значением
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Private метод без возвращаемого значения
+        private static void CheckNumber(int num)
+        {
+            if (num > 0)
+                Console.WriteLine("Число положительное");
+            else if (num < 0)
+                Console.WriteLine("Число отрицательное");
+            else
+                Console.WriteLine("Число равно нулю");
+        }
+    }
+
+    // Класс Пользователь
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"ID: {Id}, Имя: {Name}, Email: {Email}, Возраст: {Age}");
+        }
+
+        public string GetUserData()
+        {
+            return $"ID: {Id}, Имя: {Name}, Email: {Email}, Возраст: {Age}";
+        }
+    }
+}
